d9547d4ae27abb5b76002cf6d38c378e2d1c329e
Ashim Gelal <gelalasim@gmail.com>
2021-07-14 11:07:49 +0545

    second iteration
    Adding images and fetching from  github api

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-//Creating a request variable and a new XMLHttp Request
-// object to it.
-var request = new XMLHttpRequest()
-    //open a new connection, using the GET request ont he URL endpoint
-request.open('GET', 'https://api.github.com/users/asGelal/repos', true)
-request.onload = function() {
-    var data = JSON.parse(this.response);
+const gitapi_url = 'https://api.github.com/users/asGelal/repos';
+async function getrepo() {
+    const response = await fetch(gitapi_url);
+    const data = await response.json();
 
+    let l = data.length;
 
+    var leng = parseInt(l);
+
+    for (i = 0; i < leng; i++) {
+        let repoName = "";
+        repoName = data[i]['name'];
+        // console.log(repoName);
+        switch (repoName) {
+            case "-The-Python-Workshop":
+                repoUrlP = data[i]['html_url'];
+                document.getElementById("TPW").href = repoUrlP;
+                // console.log(repoUrl);
+                break;
+            case "Travery_webdev":
+                repoUrlT = data[i]['html_url'];
+                document.getElementById("TP").href = repoUrlT;
+                // console.log(repoUrl);
+                break;
+            case "Portfolio":
+                repoUrlPf = data[i]['html_url'];
+                document.getElementById("Portf").href = repoUrlPf;
+
+                // console.log(repoUrl);
+                break;
+            default:
+                continue;
+        }
+    }
 
 
 }
-request.send();
+getrepo();
